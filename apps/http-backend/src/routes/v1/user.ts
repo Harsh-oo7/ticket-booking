a23405cc5f92ea489dc9f235552ad1ad8ee13d86
Harsh-oo7 <hkaneria22@gmail.com>
2025-01-26 16:22:57 +0530

    test cases for signin

--- apps/http-backend/src/routes/v1/user.ts
+++ apps/http-backend/src/routes/v1/user.ts
@@ -7,89 +7,123 @@ import { getTOTP, verifyTOTP } from "../../utils/totp";
 const router = Router();
 
 router.post("/signup", async (req, res) => {
-    const phoneNumber = req.body.phoneNumber;
-    const totp = getTOTP(phoneNumber, 'SIGNUP');
-    console.log("totp========>", totp);
+    try {
+        const phoneNumber = req.body.phoneNumber;
+        const totp = getTOTP(phoneNumber, 'SIGNUP');
+        console.log("totp========>", totp);
 
-    // send otp to phoneNumber
+        // send otp to phoneNumber
 
-    const user = await client.user.upsert({
-        where: {
-            number: phoneNumber
-        },
-        create: {
-            number: phoneNumber,
-            name: ""
-        },
-        update: {
+        const user = await client.user.upsert({
+            where: {
+                number: phoneNumber
+            },
+            create: {
+                number: phoneNumber,
+                name: ""
+            },
+            update: {
 
-        }
-    })
+            }
+        })
 
-    res.json({
-        id: user.id
-    })
+        res.json({
+            id: user.id
+        })
+    }
+    catch(error) {
+        res.status(500).send('Internal Server Error');
+    }
 })
 
 
 router.post("/signup/verify", async (req, res) => {
-    const phoneNumber = req.body.phoneNumber;
-    const name = req.body.name;
+    try {
+        const phoneNumber = req.body.phoneNumber;
+        const name = req.body.name;
 
-    if(process.env.NODE_ENV === 'production' && !verifyTOTP(phoneNumber, req.body.otp, 'SIGNUP')) {
-        res.status(401).send('Invalid OTP');
-        return;
-    }
-
-    const user = await client.user.update({
-        where: {
-            number: phoneNumber
-        },
-        data: {
-            name: name,
-            verified: true
+        if(process.env.NODE_ENV === 'production' && !verifyTOTP(phoneNumber, req.body.otp, 'SIGNUP')) {
+            res.status(401).send('Invalid OTP');
+            return;
         }
-    })
 
-    const token = jwt.sign({
-        userId: user.id
-    }, JWT_PASSWORD)
+        const user = await client.user.update({
+            where: {
+                number: phoneNumber
+            },
+            data: {
+                name: name,
+                verified: true
+            }
+        })
 
-    res.json({
-        token
-    })
+        const token = jwt.sign({
+            userId: user.id
+        }, JWT_PASSWORD)
+
+        res.json({
+            token
+        })
+    }
+    catch(error) {
+        res.status(500).send('Internal Server Error');
+    }
 })
 
 router.post("/signin", async (req, res) => {
-    const phoneNumber = req.body.phoneNumber;
-    const totp = getTOTP(phoneNumber, 'SIGNIN');
-    console.log("totp========>", totp);
-
-    res.json({
-        message: "OTP sent"
-    })
+    try {
+        const phoneNumber = req.body.phoneNumber;
+        const user = await client.user.findFirst({
+            where: {
+                number: phoneNumber
+            }
+        })
+        if(!user) {
+            res.status(404).send('User not found');
+            return;
+        }
+        const totp = getTOTP(phoneNumber, 'SIGNIN');
+        console.log("totp========>", totp);
+    
+        res.json({
+            message: "OTP sent"
+        })
+    }
+    catch(error) {
+        res.status(500).send('Internal Server Error');
+    }
+    
 })
 
 router.post("/signin/verify", async (req, res) => {
-    const number = req.body.phoneNumber;
-    if(process.env.NODE_ENV === 'production' && !verifyTOTP(number, req.body.otp, 'SIGNIN')) {
-        res.status(401).send('Invalid OTP');
-        return;
-    }
-
-    const user = await client.user.findFirst({
-        where: {
-            number
+    try {
+        const number = req.body.phoneNumber;
+        if(process.env.NODE_ENV === 'production' && !verifyTOTP(number, req.body.otp, 'SIGNIN')) {
+            res.status(401).send('Invalid OTP');
+            return;
         }
-    })
 
-    const token = jwt.sign({
-        userId: user.id
-    }, JWT_PASSWORD)
+        const user = await client.user.findFirst({
+            where: {
+                number
+            }
+        })
+        if(!user) {
+            res.status(404).send('User not found');
+            return;
+        }
 
-    res.json({
-        token
-    })
+        const token = jwt.sign({
+            userId: user.id
+        }, JWT_PASSWORD)
+
+        res.json({
+            token
+        })
+    }
+    catch(error) {
+        res.status(500).send('Internal Server Error');
+    }
 })
 
 export default router;
--- packages/tests/index.spec.ts
+++ packages/tests/index.spec.ts
@@ -8,24 +8,45 @@ const NAME1 = "Harsh"
 
 describe("Signup Endpoints", () => {
 
-    it('Double signup doesnt work', async () => {
+    it('Signup works as expected', async () => {
         const res1 = await axios.post(`${BACKEND_URL}/api/v1/user/signup`, {
             phoneNumber: PHONE1
         })
 
         const res2 = await axios.post(`${BACKEND_URL}/api/v1/user/signup/verify`, {
             phoneNumber: PHONE1,
-            otp: '000000'
+            otp: '000000',
+            name: NAME1
         })
 
         expect(res1.status).toBe(200);
         expect(res2.status).toBe(200);
         expect(res1.data.id).not.toBeNull();
+    })
+})
 
-        // expect(async() => {
-        //     await axios.post(`${BACKEND_URL}/api/v1/user/signup`, {
-        //         number: PHONE1
-        //     })
-        // }).toThrow();
+
+describe("Signin Endpoints", () => {
+
+    it('Signin works as expected', async () => {
+        const res1 = await axios.post(`${BACKEND_URL}/api/v1/user/signin`, {
+            phoneNumber: PHONE1
+        })
+
+        const res2 = await axios.post(`${BACKEND_URL}/api/v1/user/signin/verify`, {
+            phoneNumber: PHONE1,
+            otp: '000000',
+        })
+
+        expect(res1.status).toBe(200);
+        expect(res2.status).toBe(200);
+        expect(res2.data.token).not.toBeNull();
+
+    })
+
+    it('Signin doesnt work when user doesnt exist', async () => {
+        await expect(axios.post(`${BACKEND_URL}/api/v1/user/signin`, {
+            phoneNumber: "1234567890"
+        })).rejects.toThrowError();
     })
 })
